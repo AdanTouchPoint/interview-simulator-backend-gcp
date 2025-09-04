ec438722c45bf4aa243d60a9c1b2a7717d52eddf
Adan <adan.mijangos@touchpointmarketing.mx>
2025-09-04 15:47:08 -0600

    fixed createsession endpoint

--- src/routes/dashBoardContent.ts
+++ src/routes/dashBoardContent.ts
@@ -4,18 +4,16 @@ import { createSession,updateSession } from "../controllers/sessions";
 
 const router = Router();
 
-router.post("/sessions", async (req, res) => {
+router.post("/create-session", async (req, res) => {
     try {
-      interface MyQuery extends ParsedQs {
-        info: string;
-      }
-      const query = req.query as MyQuery;
-      const objReady = JSON.parse(query.info);
-      const data = await createSession(objReady)
+      const body = req.body
+      //const objReady = JSON.parse(body);
+      const data = await createSession(body)
+      console.log(data, "data")
       res.json({
         success: true,
         message: "campaign create done",
-        
+        data: data
       });
     } catch (error) {
       res.status(400);
@@ -25,8 +23,6 @@ router.post("/sessions", async (req, res) => {
       });
     }
 });
-
-
 
 router.put("/updateSession", async (req, res) => {
     try {
@@ -46,6 +42,7 @@ router.put("/updateSession", async (req, res) => {
       });
     }
 });
+
 router.post("/media", async (req, res) => {
     try {
       interface MyQuery extends ParsedQs {
