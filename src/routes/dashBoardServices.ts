b90e765a599d81ddaae709feb9f59508b353fbdd
Adan <adan.mijangos@touchpointmarketing.mx>
2025-09-17 18:09:37 -0600

    fixed newStorage function

--- src/routes/dashBoardServices.ts
+++ src/routes/dashBoardServices.ts
@@ -9,6 +9,10 @@ const router = Router();
 
 const storage = new Storage({
   projectId: process.env.GCP_PROJECT_ID,
+  credentials: {
+    client_email: process.env.GCP_CLIENT_EMAIL,
+    private_key: process.env.GCP_PRIVATE_KEY ? process.env.GCP_PRIVATE_KEY.replace(/\\n/g, '\n') : undefined,
+  },
 });
 
 const bucket = storage.bucket(process.env.GCS_BUCKET_NAME || 'interview-sim-uploads-raw');
